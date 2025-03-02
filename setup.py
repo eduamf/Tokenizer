2b4447c9514a5c3057deda4a085014f097b0dbe0
Mello <eduardo.freitas@camara.leg.br>
2025-03-02 13:18:29 -0300

    Create setup.py
    Preparing to PIP

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tokenizer-eamf",  # Nome do pacote no PyPI (escolha um único)
+    version="0.1.0",  # Versão inicial do pacote
+    author="Eduardo A M Freitas",  # Nome do autor
+    author_email="eduardo.freitas@camara.leg.br",  # Email do autor
+    description="A simple tokenizer to NLP.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/eduamf/Tokenizer",
+    packages=find_packages(exclude=["tests*", "docs*"]),  # Encontra e inclui pacotes automaticamente
+    install_requires=[],  # Dependências (adicione se necessário)
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
