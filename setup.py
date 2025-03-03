00f6e5002f0583d0930ca46633a49a31bfa09dd5
Mello <eduardo.freitas@camara.leg.br>
2025-03-03 20:48:28 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mypytok",  # Nome do pacote no PyPI (escolha um único)
-    version="0.1.0",  # Versão inicial do pacote
+    name="pyonmttok-amf",  # Nome do pacote no PyPI (escolha um único)
+    version="1.37.",      # Versão inicial do pacote
     author="Eduardo A M Freitas",  # Nome do autor
     author_email="eduardo.freitas@camara.leg.br",  # Email do autor
-    description="A simple tokenizer to NLP.",
+    description="Tokenizer to NLP.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/eduamf/Tokenizer",
